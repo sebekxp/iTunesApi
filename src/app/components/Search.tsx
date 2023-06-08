082032a19995ba87f5d7bfabe10f28f414c1b3a6
Sebastian Paszynski <sebastian.paszynski@sabre.com>
2023-06-09 01:45:39 +0200

    Implemeneted Search componenet

---
+++ src/app/components/Search.tsx
@@ -0,0 +1,54 @@
+'use client';
+import { ChangeEvent, useState } from 'react';
+import { useRecoilState } from 'recoil';
+import { searchQueryState } from '../store/atoms';
+
+export const Search = () => {
+  const [_, setQuery] = useRecoilState(searchQueryState);
+  const [value, setValue] = useState('');
+
+  const onChange = (event: ChangeEvent<HTMLInputElement>) => {
+    const inputValue = event.target.value;
+    setValue(inputValue);
+    setQuery(inputValue);
+  };
+
+  return (
+    <form className="w-full">
+      <label
+        htmlFor="default-search"
+        className="mb-2 text-sm font-medium text-gray-900 sr-only dark:text-white"
+      >
+        Search
+      </label>
+      <div className="relative">
+        <div className="absolute inset-y-0 left-0 flex items-center pl-3 pointer-events-none">
+          <svg
+            aria-hidden="true"
+            className="w-5 h-5 text-[color:var(--redPrimary)]"
+            fill="none"
+            stroke="currentColor"
+            viewBox="0 0 24 24"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z"
+            ></path>
+          </svg>
+        </div>
+        <input
+          type="search"
+          id="default-search"
+          className="block w-full p-4 pl-10 text-sm rounded-lg bg-[color:var(--greySecondary)] focus:border-[color:var(--greySecondary)] ring-black"
+          placeholder="Search..."
+          required
+          value={value}
+          onChange={onChange}
+        />
+      </div>
+    </form>
+  );
+};
