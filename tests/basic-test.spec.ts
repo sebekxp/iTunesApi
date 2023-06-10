ff1e45d793b0d983a37da16aaf32506fcfecc6bd
Sebastian Paszynski <sebastian.paszynski@sabre.com>
2023-06-10 14:02:10 +0200

    Changed approach to testing not using hard-coded data, so if the top albums list changes, the test should not be changed

--- tests/basic-test.spec.ts
+++ tests/basic-test.spec.ts
@@ -1,11 +1,19 @@
 import { test, expect, BrowserType, chromium, firefox } from '@playwright/test';
 
+const PLAYER_REGEXP = /^(http:\/\/localhost:3000\/player\/)/;
+const ANY_TEKST_REGEXP = /.*/;
+
 test('should navigate to the album page after click on album', async ({ page }) => {
   await page.goto('/');
-  await page.click('text="But Here We Are"');
+  await page.waitForSelector('[data-testid="card-element"]');
 
-  await expect(page).toHaveURL('http://localhost:3000/player/but-here-we-are');
-  await expect(page.locator('h1')).toContainText('But Here We Are');
+  const cardElements = await page.$$('[data-testid="card-element"]');
+
+  // Click on the second album from the list
+  cardElements[0].click();
+
+  await expect(page).toHaveURL(PLAYER_REGEXP);
+  await expect(page.locator('h1')).toContainText(ANY_TEKST_REGEXP);
 });
 
 test('should have the expected number of items that will be returned from the API', async ({
@@ -35,13 +43,15 @@ test('should search for album name and filter list', async ({ browserName, page 
 
     const input = page.getByTestId('default-search');
 
-    await input.fill('But Here We Are');
+    // Fill with eee to make filter work
+    await input.fill('ee');
 
     await page.waitForSelector('[data-testid="card-element"]');
 
     const cardElements = await page.$$('[data-testid="card-element"]');
 
-    expect(cardElements.length).toBe(1);
+    // The filter works if the value is different from the original returned by API
+    expect(cardElements.length).not.toBe(74);
     await browser.close();
   } else {
     // Skip tests for WebKit, known issue:
@@ -58,21 +68,34 @@ test('should navigate to the album page after click and then click on the next b
   await page.waitForSelector('[data-testid="card-element"]');
 
   const cardElements = await page.$$('[data-testid="card-element"]');
-
   // Click on the second album from the list
   cardElements[1].click();
 
   // Verify correctness
-  await expect(page).toHaveURL('http://localhost:3000/player/life-is-but-a-dream');
-  await expect(page.locator('h1')).toContainText('Life Is But a Dream…');
+  await expect(page).toHaveURL(PLAYER_REGEXP);
+  await expect(page.locator('h1')).toContainText(ANY_TEKST_REGEXP);
 
+  const currentAlbumUrl = await page.url();
+
+  await page.waitForSelector('[data-testid="button-next"]');
   // Click on the next button to switch to next album
   const buttonNext = await page.$$('[data-testid="button-next"]');
   buttonNext[0].click();
 
+  // Wait for the URL to change
+  await page.waitForFunction(
+    (currentAlbumUrl) => location.href !== currentAlbumUrl,
+    currentAlbumUrl,
+  );
+
   // Verify correctness
-  await expect(page).toHaveURL('http://localhost:3000/player/5-star');
-  await expect(page.locator('h1')).toContainText('5-STAR');
+  await expect(page).toHaveURL(PLAYER_REGEXP);
+  await expect(page.locator('h1')).toContainText(ANY_TEKST_REGEXP);
+
+  const nextAlbumUrl = await page.url();
+
+  // Check if the url changed after sqtching to the new page
+  await expect(currentAlbumUrl).not.toBe(nextAlbumUrl);
 });
 
 test('should navigate to the album page after click and then click on the prev button to switch to the prev album', async ({
@@ -87,8 +110,12 @@ test('should navigate to the album page after click and then click on the prev b
   cardElements[1].click();
 
   // Verify correctness
-  await expect(page).toHaveURL('http://localhost:3000/player/life-is-but-a-dream');
-  await expect(page.locator('h1')).toContainText('Life Is But a Dream…');
+  await expect(page).toHaveURL(PLAYER_REGEXP);
+  await expect(page.locator('h1')).toContainText(ANY_TEKST_REGEXP);
+
+  const currentAlbumUrl = await page.url();
+
+  await page.waitForSelector('[data-testid="button-prev"]');
 
   // Click on the prev button to switch to prev album
   const buttonPrev = await page.$$('[data-testid="button-prev"]');
@@ -96,9 +123,20 @@ test('should navigate to the album page after click and then click on the prev b
   // to switch albums, not to the root
   buttonPrev[1].click();
 
+  // Wait for the URL to change
+  await page.waitForFunction(
+    (currentAlbumUrl) => location.href !== currentAlbumUrl,
+    currentAlbumUrl,
+  );
+
   // Verify correctness
-  await expect(page).toHaveURL('http://localhost:3000/player/but-here-we-are');
-  await expect(page.locator('h1')).toContainText('But Here We Are');
+  await expect(page).toHaveURL(PLAYER_REGEXP);
+  await expect(page.locator('h1')).toContainText(ANY_TEKST_REGEXP);
+
+  const prevAlbumUrl = await page.url();
+  console.log(currentAlbumUrl, prevAlbumUrl);
+  // Check if the url changed after sqtching to the new page
+  await expect(currentAlbumUrl).not.toBe(prevAlbumUrl);
 });
 
 test('should navigate to the album page after click and then click on the back button to switch the root', async ({
@@ -113,9 +151,10 @@ test('should navigate to the album page after click and then click on the back b
   cardElements[1].click();
 
   // Verify correctness
-  await expect(page).toHaveURL('http://localhost:3000/player/life-is-but-a-dream');
-  await expect(page.locator('h1')).toContainText('Life Is But a Dream…');
+  await expect(page).toHaveURL(PLAYER_REGEXP);
+  await expect(page.locator('h1')).toContainText(ANY_TEKST_REGEXP);
 
+  await page.waitForSelector('[data-testid="button-prev"]');
   // Click on the prev button to switch to prev album
   const buttonPrev = await page.$$('[data-testid="button-prev"]');
   // There are two left buttons, we are interested in the first one to switch to the root
